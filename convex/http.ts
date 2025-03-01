6cce5c14f715989d309f03dd11ab077f180d2c11
Ninjasri98 <palitsriparno@gmail.com>
2025-03-01 11:06:00 +0530

    added schemas and set up queries

--- convex/http.ts
+++ convex/http.ts
@@ -1,5 +1,9 @@
 import { httpRouter } from "convex/server";
 import { httpAction } from "./_generated/server";
+import {Webhook} from "svix";
+import {WebhookEvent} from "@clerk/nextjs/server";
+import {api} from "./_generated/api";
+
 
 const http = httpRouter();
 
@@ -15,5 +19,52 @@ http.route({
         const svix_signature = request.headers.get("svix-signature");
         const svix_timestamp = request.headers.get("svix-timestamp");
 
+        if(!svix_id || !svix_signature || !svix_timestamp){
+            return new Response(
+                "Missing required headers",
+                {status: 400}
+            );
+        }
+        const payload = await request.json();
+        const body = JSON.stringify(payload);
+
+        const wh = new Webhook(webhookSecret);
+        let evt : WebhookEvent;
+
+        try {
+            evt = wh.verify(body,{
+                "svix-id": svix_id,
+                "svix-signature": svix_signature,
+                "svix-timestamp": svix_timestamp
+            }) as WebhookEvent;
+        } catch (error) {
+            console.error("Webhook verification failed", error);
+            return new Response("Error Occurred", {status: 400});
+        }
+
+
+        const eventType = evt.type;
+
+        if(eventType === "user.created"){
+            const { id, email_addresses, first_name, last_name, image_url } = evt.data;
+
+            const email = email_addresses[0].email_address;
+            const name = `${first_name || ""} ${last_name || ""}`.trim();
+
+            try {
+                await ctx.runMutation(api.users.syncUser,{
+                    clerkId : id,
+                    email,
+                    name,
+                    image: image_url
+                })
+            } catch (error) {
+                console.error("Error Creating User ", error);
+                return new Response("Error Occurred", {status: 500});
+            }
+        }
+        return new Response("Webhook processed successfully", {status: 200});
     })
 })
+
+export default http;
