7e4541331d57c5f4a89d6ab3be2be70f0ae56b15
Ninjasri98 <palitsriparno@gmail.com>
2025-03-03 19:22:35 +0530

    Meeting Setup

--- src/components/MeetingSetup.tsx
+++ src/components/MeetingSetup.tsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import { useCall } from '@stream-io/video-react-sdk';
+import React, { useEffect, useState } from 'react'
 
-function MeetingSetup() {
+function MeetingSetup({onSetupComplete}:{onSetupComplete:()=>void}) {
+
+    const [isCameraDisabled, setIsCameraDisabled] = useState(true);
+    const [isMicDisabled, setIsMicDisabled] = useState(false);
+
+    const call = useCall()
+
+
+    if(!call) return null;
+
+    useEffect(() => {
+        if (isCameraDisabled) call.camera.disable();
+        else call.camera.enable();
+      }, [isCameraDisabled, call.camera]);
+    
+    useEffect(() => {
+        if (isMicDisabled) call.microphone.disable();
+        else call.microphone.enable();
+      }, [isMicDisabled, call.microphone]);
+
+    const handleJoin = async () =>{
+        await call.join()
+        onSetupComplete();
+    }
   return (
     <div>MeetingSetup</div>
   )
