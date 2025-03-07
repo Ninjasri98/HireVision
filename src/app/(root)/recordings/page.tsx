7b1269b0c321178ba7f32060e2de97c65ada51a6
Ninjasri98 <palitsriparno@gmail.com>
2025-03-07 19:39:11 +0530

    added recordings section and other utils

--- src/app/(root)/recordings/page.tsx
+++ src/app/(root)/recordings/page.tsx
@@ -1,6 +1,36 @@
-import React from 'react'
+"use client";
+
+import LoaderUI from "@/components/LoaderUI";
+import useGetCalls from "@/hooks/useGetCalls";
+import { CallRecording } from "@stream-io/video-react-sdk";
+import { useEffect, useState } from "react";
 
 function RecordingsPage() {
+  const {calls,isLoading} = useGetCalls()
+  const [recordings, setRecordings] = useState<CallRecording[]>([]);
+
+  useEffect(()=>{
+    const fetchRecordings = async () => {
+      if (!calls) return;
+
+      try {
+        // Get recordings for each call
+        const callData = await Promise.all(calls.map((call) => call.queryRecordings()));
+        const allRecordings = callData.flatMap((call) => call.recordings);
+
+        setRecordings(allRecordings);
+      } catch (error) {
+        console.log("Error fetching recordings:", error);
+      }
+    };
+
+    fetchRecordings();
+
+  },[calls])
+
+  if (isLoading) return <LoaderUI />;
+
+  
   return (
     <div>RecordingsPage</div>
   )
