c03d8d383f5b271671b218efde62b2d568a8e8b1
Ninjasri98 <palitsriparno@gmail.com>
2025-03-08 18:46:01 +0530

    started with dashboard page

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,6 +1,32 @@
+"use client";
+import { useMutation, useQuery } from 'convex/react';
 import React from 'react'
+import { api } from '../../../../convex/_generated/api';
+import { Id } from '../../../../convex/_generated/dataModel';
+import toast from 'react-hot-toast';
+import LoaderUI from '@/components/LoaderUI';
+import { groupInterviews } from '@/lib/utils';
+
 
 function DashboardPage() {
+  const users = useQuery(api.users.getUsers)
+  const interviews = useQuery(api.interviews.getAllInterviews)
+  const updateStatus = useMutation(api.interviews.updateInterviewStatus)
+
+  const handleStatusUpdate = async (InterviewId : Id<"interviews"> , status : string ) =>{
+    try {
+      await updateStatus({id:InterviewId,status})
+      toast.success(`Interview marked as ${status}`)
+    } catch (error) {
+      toast.error("Failed to update status")
+    }
+  }
+
+  if (!interviews || !users) return <LoaderUI/>
+
+  const groupedInterviews = groupInterviews(interviews);
+
+  
   return (
     <div>DashboardPage</div>
   )
